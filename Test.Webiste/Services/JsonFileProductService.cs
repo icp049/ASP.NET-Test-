a45e854f22cf23a5ff260775a4e708f1389ca8ec
ipedeglorio <ipedeglorio@ESLAP5>
2024-02-01 13:24:36 -0600

    Add project files.

---
+++ Test.Webiste/Services/JsonFileProductService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
+using Test.Webiste.Models;
+
+namespace Test.Webiste.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using var jsonFileReader = File.OpenText(JsonFileName);
+
+            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+    }
+}
